e035e225e0e03a71c66dcef6794626320966b7fa
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-10-05 13:56:12 +0800

    t

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import { authOptions } from "./api/auth/[...nextauth]/options";
 export default async function Home() {
   const data = await getServerSession(authOptions);
   return (
+    //te
     <>
       <InViewSection />
       <Navbar isBordered>
